e6b8028e4b9bc70fb6520ca3b9335f48b0a47105
jendevane <87095302+jendevane@users.noreply.github.com>
2021-09-05 03:00:28 -0400

    finished add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,12 @@
 console.dir (window.document);
 var buttonE1=document.querySelector("#save-task");
 var taskstodoE1=document.querySelector("#tasks-to-do");
-buttonE1.addEventListener("click", function() {
+var createTaskHandler=function(){
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  });
+    taskstodoE1.appendChild(listItemEl);
+     };
+     buttonE1.addEventListener("click", createTaskHandler);
+    
