dbc6d4e4d2d666c0e17da0e1f611479763261db4
jendevane <87095302+jendevane@users.noreply.github.com>
2021-09-05 14:07:48 -0400

    task-bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 
 console.dir (window.document);
-var buttonE1=document.querySelector("#save-task");
+var formE1=document.querySelector("#task-form");
 var taskstodoE1=document.querySelector("#tasks-to-do");
-var createTaskHandler=function(){
-
+var createTaskHandler=function(event){
+    event.preventDefalt();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     taskstodoE1.appendChild(listItemEl);
+    var taskNameInput = document.querySelector("input[name='task-name']");
+    console.log(taskNameInput);
      };
-     buttonE1.addEventListener("click", createTaskHandler);
+     formE1= addEventListener("submit", createTaskHandler);
     
