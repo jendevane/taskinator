f0896e6fd18345d954984fbd9333f99cce8c2d3c
jendevane <87095302+jendevane@users.noreply.github.com>
2021-09-06 22:52:50 -0400

    task

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,12 @@ console.dir (window.document);
 var formE1=document.querySelector("#task-form");
 var taskstodoE1=document.querySelector("#tasks-to-do");
 var createTaskHandler=function(event){
-    event.preventDefalt();
+    event.preventDefault();
+    var taskNameInput= document.querySelector ("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "tasknameInput";
+    listItemEl.textContent = "taskNameInput";
     taskstodoE1.appendChild(listItemEl);
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     console.dir(taskNameInput);
