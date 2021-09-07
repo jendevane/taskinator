227732aefbb86ab878c7af362cde9e665e433ada
jendevane <87095302+jendevane@users.noreply.github.com>
2021-09-07 00:55:06 -0400

    fixed error

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,15 @@ var createTaskHandler=function(event){
     event.preventDefault();
     var taskNameInput= document.querySelector ("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log (taskTypeInput)
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     var taskInfoEl=document.createElement("div");
         taskInfoEl.className="task-info";
-        taskInfoEl.innerHTML="<h3 class='task-name'>" + taskNameInput +"</h3><span class='task-type'>" + taskTypeInput +"</span>"
+        taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
         listItemEl.appendChild(taskInfoEl);
         tasksToDoEl.appendChild(listItemEl);
-    listItemEl.textContent = taskNameInput
-    tasksToDoEl.appendChild(listItemEl);
-    var taskNameInput = document.querySelector("input[name='task-name']").value;
-    console.dir(taskNameInput);
+ 
      };
      formEl= addEventListener("submit", createTaskHandler);
     
